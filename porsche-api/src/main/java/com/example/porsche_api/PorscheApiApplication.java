926c693f8a639d7cd0c774a3ab4b177f703e01f3
dandev004 <crivoidan2004@gmail.com>
2026-06-12 19:26:58 +0300

    create structure project and install all dependencies neccessary

---
+++ porsche-api/src/main/java/com/example/porsche_api/PorscheApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.porsche_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorscheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorscheApiApplication.class, args);
+	}
+
+}
---
+++ porsche-api/src/test/java/com/example/porsche_api/PorscheApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.porsche_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorscheApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
