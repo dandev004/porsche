f3a9d15f83fd6d81b730a7212aba74f3b42b534d
dandev004 <crivoidan2004@gmail.com>
2026-06-12 23:12:33 +0300

    finish hero page and add images in supabase

--- porsche-client/src/components/Header.tsx
+++ porsche-client/src/components/Header.tsx
@@ -3,21 +3,26 @@ import { GrLanguage } from "react-icons/gr";
 import { Link } from "react-router-dom";
 import { GoPerson } from "react-icons/go";
 
-
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full z-50">
-      <div className="flex items-center justify-between px-4 sm:px-8 py-4 ">
+      
+      <div className="relative flex items-center justify-between px-4 sm:px-8 py-6">
+
         <RiMenuLine size={18} color="white" />
-        <Link to="/">
-          <img src="/logo.png" alt="logo" width={30} height={30} />
+        <Link to="/" className="absolute left-1/2 -translate-x-1/2">
+          <img src="/logo-desktop.png" alt="logo"className="hidden md:block w-[260px]"/>
+          <img src="/logo-mobile.png" alt="logo" className="md:hidden w-7.5"
+          />
         </Link>
+
         <div className="flex gap-4">
-          <GrLanguage size={18} color="white"/>
+          <GrLanguage size={18} color="white" />
           <GoPerson size={18} color="white" />
         </div>
+
       </div>
     </header>
-  )
-}
+  );
+};
 export default Header
--- porsche-client/src/pages/Hero.tsx
+++ porsche-client/src/pages/Hero.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
-
+import { motion } from "framer-motion"
+import { useRef, useState } from "react";
+import { IoPause } from "react-icons/io5";
+import { IoPlay } from "react-icons/io5";
 const Hero = () => {
+  const [isPlaying, setIsPlaying] = useState(true)
+  const videoRef = useRef(null);
+
+
+  const togglePlay = () => {
+    const video = videoRef.current;
+    if (!video) return;
+    if (isPlaying) {
+      video.pause();
+      setIsPlaying(false)
+    } else {
+      video.play();
+      setIsPlaying(true)
+    }
+  }
   return (
-     <section className="relative h-[100dvh] w-full overflow-hidden">
-      <video autoPlay muted loop playsInline className="absolute top-0 left-0 w-full h-full object-cover">
-        <source src="/video-hero.mp4" type="video/mp4" />
+    <section className="relative h-dvh w-full overflow-hidden">
+      <video ref={videoRef} autoPlay muted loop playsInline
+             className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source
+          src="https://vrvqjmkllbxtcjxxhiju.supabase.co/storage/v1/object/public/porsche/hero-desktop.mp4"
+          media="(min-width: 768px)"
+        />
+        <source
+          src="https://vrvqjmkllbxtcjxxhiju.supabase.co/storage/v1/object/public/porsche/hero-mobile.mp4"
+          media="(max-width: 767px)"
+        />
       </video>
+
+      <div className='absolute px-6 bottom-1/6 flex flex-col justify-end gap-4 lg:w-1/3 transition-all duration-700 ease-in-out'>
+        <motion.h1
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="text-3xl md:text-5xl lg:text-6xl text-white tracking-wide"
+        >
+          Sonderwunsch "Tree of Life"
+        </motion.h1>
+        <button 
+          className='w-full p-4 bg-white/20 backdrop-blur-md rounded-lg text-white text-[16px] md:w-[35%] 
+                     lg:w-[50%] xl:w-[40%] font-semibold hover:bg-white/10 '
+        >
+          Discover more
+        </button>
+      </div>
+
+      <div
+        onClick={togglePlay}
+        className="absolute bottom-10 right-6 w-12 h-12 bg-white/20 backdrop-blur-sm 
+                   rounded-full flex items-center justify-center cursor-pointer"
+      >
+        {isPlaying ? (
+          <IoPause size={20} color="white" />
+        ) : (
+          <IoPlay size={20} color="white" />
+        )}
+      </div>
 
     </section>
   )
